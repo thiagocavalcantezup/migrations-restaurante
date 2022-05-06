4671cf7c268e4ad5509de2f937e6100dfb39373a
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-05-06 13:30:56 -0300

    Add restaurant entity and migration

---
+++ src/main/java/br/com/zup/handora/migrationsrestaurante/restaurante/Restaurante.java
@@ -0,0 +1,77 @@
+package br.com.zup.handora.migrationsrestaurante.restaurante;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "restaurantes")
+public class Restaurante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nomeFantasia;
+
+    @Column(nullable = false, length = 18)
+    private String cnpj;
+
+    @Column(nullable = false, length = 120)
+    private String endereco;
+
+    @Column(nullable = false, length = 60)
+    private String bairro;
+
+    @Column(nullable = false, length = 60)
+    private String cidade;
+
+    @Column(nullable = false, length = 60)
+    private String estado;
+
+    @Column(nullable = false, length = 9)
+    private String cep;
+
+    @Column(length = 20)
+    private String telefone;
+
+    @Column(length = 20)
+    private String celular;
+
+    @Column(length = 60)
+    private String email;
+
+    @Column(length = 120)
+    private String site;
+
+    /**
+     * @deprecated Construtor de uso exclusivo do Hibernate
+     */
+    @Deprecated
+    public Restaurante() {}
+
+    public Restaurante(String nomeFantasia, String cnpj, String endereco, String bairro,
+                       String cidade, String estado, String cep, String telefone, String celular,
+                       String email, String site) {
+        this.nomeFantasia = nomeFantasia;
+        this.cnpj = cnpj;
+        this.endereco = endereco;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.cep = cep;
+        this.telefone = telefone;
+        this.celular = celular;
+        this.email = email;
+        this.site = site;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+}
---
+++ src/main/resources/db/migration/V202206051327__criar_tabela_restaurantes.sql
@@ -0,0 +1,14 @@
+CREATE TABLE restaurantes (
+  id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nomeFantasia VARCHAR(255) NOT NULL,
+  cnpj VARCHAR(18) NOT NULL,
+  endereco VARCHAR(120) NOT NULL,
+  bairro VARCHAR(60) NOT NULL,
+  cidade VARCHAR(60) NOT NULL,
+  estado VARCHAR(60) NOT NULL,
+  cep VARCHAR(9) NOT NULL,
+  telefone VARCHAR(20),
+  celular VARCHAR(20),
+  email VARCHAR(60),
+  site VARCHAR(120)
+)
