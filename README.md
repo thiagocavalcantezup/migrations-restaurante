75097fc68dcd64191efd31f8e5b0e1bc5793a72f
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-05-06 13:15:19 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Migrations
+
+## Adicionando Flywaydb ao projeto e criando as primeiras Migrations
+
+Construa o projeto com suas próprias mãos
---
+++ src/test/java/br/com/zup/handora/migrationsrestaurante/MigrationsRestauranteApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.zup.handora.migrationsrestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationsRestauranteApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
